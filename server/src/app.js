a9a6d2f390871303d654fe80be8d58c788b3ec48
SimmSimmPopcornSnack <hiroto_o20@hotmail.com>
2021-09-02 17:16:59 +0900

    Adding user.js to src/routes and a function getLikedVideos() via api/v1/user

--- server/src/app.js
+++ server/src/app.js
@@ -4,8 +4,10 @@ var app = express();
 
 // var videoRoute = require('./routes/video.js');
 import videoRoute from './routes/video.js';
+import userRoute from './routes/user.js';
 
 app.use('/api/v1/video', videoRoute);
+app.use('/api/v1/user', userRoute);
 const port = 3001;
 app.listen(port, () => {
     console.log('listening to port:' + port);
---
+++ server/src/routes/user.js
@@ -0,0 +1,26 @@
+// server/src/routes/user.js
+
+import Prisma from "../../../node_modules/@prisma/client/index.js"
+const { PrismaClient } = Prisma
+import express from "express"
+
+// import Protect from "../middleware/authorization";
+// const { protect } = Protect;
+
+const prisma = new PrismaClient();
+
+function getUserRoutes() {
+  const router = express.Router();
+
+//   router.get("/liked-videos", protect, getLikedVideos);
+  router.get("/liked-videos", getLikedVideos);
+    
+  return router;
+}
+
+function getLikedVideos(req, res) {
+    let videos = Array(0);
+    res.status(200).json({ videos });
+}
+
+export default getUserRoutes();
